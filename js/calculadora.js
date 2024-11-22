3d8565167acbfdd32ca62dd581f8704e14a6010b
jhona166 <jdaltamiranoo@uce.edu.ec>
2024-11-22 00:38:00 +0000

    Taller10

---
+++ js/calculadora.js
@@ -0,0 +1,45 @@
+var num1 = 0;
+var num2 = 0;
+var operacion = "";
+
+function setearValor() {
+
+    let valorActual = document.getElementById("id-display").innerText = valor;
+    console.log(valorActual);
+    document.getElementById("id-display").innerText = valor;
+}
+
+function setearValorConcatenado() {
+    let valor = document.getElementById("id-display").innerText = valor;
+    console.log(valorActual);
+    document.getElementById("id-display").innerText = valorActual + valor;
+}
+
+
+function sumar(num1, num2) {
+    return num1 + num2
+}
+function sumarNumeros() {
+    var numero1 = convertir('idNum1')
+    var numero2 = convertir('idNum2')
+    document.getElementById('labelResultado').innerHTML = 'Resultado: ' + sumar(numero1, numero2)
+}
+function restar(num1, num2) {
+    return num1 - num2
+}
+function restarNumeros() {
+    var numero1 = convertir('idNum1')
+    var numero2 = convertir('idNum2')
+    document.getElementById('labelResultado').innerHTML = 'Resultado: ' + restar(numero1, numero2)
+}
+function multiplicacion(num1, num2) {
+    return num1 * num2
+}
+function multiplicarNumeros() {
+    var numero1 = convertir('idNum1')
+    var numero2 = convertir('idNum2')
+    document.getElementById('labelResultado').innerHTML = 'Resultado: ' + multiplicacion(numero1, numero2)
+}
+function convertir(idCampo) {
+    return parseInt(document.getElementById(idCampo).value)
+}
