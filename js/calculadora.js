99a5e08e7eddea362a1d359691ba76c7d7001f11
Jhonatan Altamirano <jdaltamiranoo1@uce.edu.ec>
2024-11-23 19:53:36 -0500

    Calculadora revisar

--- js/calculadora.js
+++ js/calculadora.js
@@ -2,44 +2,88 @@ var num1 = 0;
 var num2 = 0;
 var operacion = "";
 
-function setearValor() {
-
-    let valorActual = document.getElementById("id-display").innerText = valor;
+function setearValor(valor) {
+    
+    let valorActual = document.getElementById("id-display").innerText;
     console.log(valorActual);
-    document.getElementById("id-display").innerText = valor;
+   
+    document.getElementById("id-display").innerText =valor;
 }
 
-function setearValorConcatenado() {
-    let valor = document.getElementById("id-display").innerText = valor;
+function setearValorConcatenado(valor) {
+    let valorActual = document.getElementById("id-display").innerText;
     console.log(valorActual);
-    document.getElementById("id-display").innerText = valorActual + valor;
+    document.write(valor+valorActual);
+    console.log("Meinst"+ document.write(valor,valorActual))
+    document.getElementById("id-display").innerText = ''+valorActual+valor;
+}
+
+function recibirValor(valor){
+    console.log(valor);
+    if(num1===0){
+    num1 = valor;
+    setearValor(num1)
+    }else if(operacion===""){
+        num1+=valor;
+        setearValor(num1);
+    }else{
+        num2 = parseFloat(valor);
+        setearValorConcatenado(valor) 
+    }
+    console.log(num1);
+
+}
+function evaluarOperacion(op){
+    operacion = op;
+    setearValorConcatenado(op);
+}
+
+function igual(){
+    let valorFinal;
+    num1 = parseFloat(num1);
+    if(operacion==='+'){
+        valorFinal = num1+num2;
+    }else if(operacion==='-'){
+        valorFinal = num1-num2;
+    }else if(operacion==="X"){
+        valorFinal = num1*num2;
+    }
+    else if(operacion==="%"){
+        if( num2===0){
+            setearValor('Error de sintaxis!')
+        return;
+        }
+        valorFinal=num1/num2;
+    }
+    setearValor(valorFinal);
+}
+
+function limpiarDisplay() {
+    num1 = 0;
+    num2 = 0;
+    operacion = '';
+    setearValor('0');
 }
 
 
-function sumar(num1, num2) {
-    return num1 + num2
+
+function eliminar(){
+    let valorActual=document.getElementById('id-display').innerText;
+    if(valorActual.length>1){
+        let valorNuevo  = valorActual.slice(0,-1);
+        setearValor(valorNuevo);
+        if(operacion===''){
+            num1= valorNuevo;
+
+        }else{
+            num2 = parseFloat(valorNuevo);
+        }
+    }else{
+        setearValor('0');
+        num1=0;
+        num2=0;
+    }
 }
-function sumarNumeros() {
-    var numero1 = convertir('idNum1')
-    var numero2 = convertir('idNum2')
-    document.getElementById('labelResultado').innerHTML = 'Resultado: ' + sumar(numero1, numero2)
-}
-function restar(num1, num2) {
-    return num1 - num2
-}
-function restarNumeros() {
-    var numero1 = convertir('idNum1')
-    var numero2 = convertir('idNum2')
-    document.getElementById('labelResultado').innerHTML = 'Resultado: ' + restar(numero1, numero2)
-}
-function multiplicacion(num1, num2) {
-    return num1 * num2
-}
-function multiplicarNumeros() {
-    var numero1 = convertir('idNum1')
-    var numero2 = convertir('idNum2')
-    document.getElementById('labelResultado').innerHTML = 'Resultado: ' + multiplicacion(numero1, numero2)
-}
-function convertir(idCampo) {
-    return parseInt(document.getElementById(idCampo).value)
-}
+
+
+ 
